70fd14467189b4f5842b93aa9b78ade150fac3cc
Weicalvin <95908234+Weicalvin@users.noreply.github.com>
2026-02-14 09:34:09 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -10,6 +10,20 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   android: {
     package: "com.vr.pro.player",
     versionCode: 1,
-    permissions: ["READ_EXTERNAL_STORAGE", "READ_MEDIA_VIDEO"]
+    permissions: [
+      "READ_EXTERNAL_STORAGE",
+      "READ_MEDIA_VIDEO"
+    ],
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#000000"
+    }
+  },
+  extra: {
+    eas: {
+      // 👇 請將你在網頁上找到的 Project ID 貼在引號內 👇
+      projectId: "8c02e732-ab2f-481c-8b11-2fac3bd1c6e5" 
+    }
   }
 });
+
